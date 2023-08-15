350ea82552bcda7a04d8ad20b92be6c5d15e0b95
Vinit Paul <paul2596@gmail.com>
2023-08-15 07:36:18 +0100

    updated

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
 import time
+import numpy as np
 
 app = Flask(__name__)
 
 print('Testing')
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('predict.html')
 
@@ -18,6 +19,7 @@ def predict():
     data = request.json 
     input_data = data.get('data')
     test_type = data.get('testType')
+    direction = data.get('direction')
     features = []
     Pitch_Deg_Mean = float(input_data.get('pitchangle')) 
     Anemo_T_Mean = float(input_data.get('windspeed'))
@@ -32,23 +34,26 @@ def predict():
             temp_T_Mean = float(input_data.get('temperature'))
             features = [Pitch_Deg_Mean, Anemo_T_Mean, WindVane_T_Mean, temp_T_Mean] 
 
-            model = joblib.load('XG_Boost_initial_features_temp_orig-data_model.sav')
-        if(test_type=='t2m_d2m_west'):
+            model = joblib.load('C:/Users/Paul Jr/Documents/predict/XG_Boost_initial_features_temp_orig-data_model.sav')
+        if(test_type=='t2m_d2m'):
             t2m = float(input_data.get('t2m'))
             d2m = float(input_data.get('d2m'))
     
             features = [Pitch_Deg_Mean, Anemo_T_Mean, WindVane_T_Mean, t2m, d2m] 
-       
-            model = joblib.load('XG_Boost_initial_features_t2m_d2m_orig-data_west-ocean_model.sav')
-        
-        if(test_type=='t2m_d2m_east'):
-            t2m = float(input_data.get('t2m'))
-            d2m = float(input_data.get('d2m'))
-    
-            features = [Pitch_Deg_Mean, Anemo_T_Mean, WindVane_T_Mean, t2m, d2m] 
-        
-            model = joblib.load('XG_Boost_initial_features_t2m_d2m_orig-data_east-sea_model.sav')
-    
+            if(direction=='west'):
+                
+                model = joblib.load('C:/Users/Paul Jr/Documents/predict/XG_Boost_initial_features_t2m_d2m_orig-data_west-ocean_model.sav')
+            elif(direction=='east'):
+                model = joblib.load('C:/Users/Paul Jr/Documents/predict/XG_Boost_initial_features_t2m_d2m_orig-data_east-sea_model.sav')
+
+        if(test_type=='obhL'):
+            obhL = float(input_data.get('obhL'))
+            features = [Pitch_Deg_Mean, Anemo_T_Mean, WindVane_T_Mean, obhL] 
+            if(direction=='west'):
+                
+                model = joblib.load('C:/Users/Paul Jr/Documents/predict/XG_Boost_initial_features_obhL_orig-data-era5-30km-obhL_west-ocean_model.sav')
+            elif(direction=='east'):
+                model = joblib.load('C:/Users/Paul Jr/Documents/predict/XG_Boost_initial_features_obhL_orig-data-era5-30km-obhL_east-sea_model.sav')
 
 
         prediction = model.predict([features])
@@ -58,5 +63,5 @@ def predict():
         return jsonify({'error': error_value})
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
